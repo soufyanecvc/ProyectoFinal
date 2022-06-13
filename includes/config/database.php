5f152067018e5c71096558ac8bdec23db7072547
soufyanecvc <soufyanecvc@gmail.com>
2022-06-13 18:04:38 +0200

    version1

---
+++ includes/config/database.php
@@ -0,0 +1,13 @@
+<?php 
+
+function conectarDB() : mysqli {
+    $db = new mysqli('localhost', 'root', '', 'bienes_raices');
+
+    if(!$db) {
+        echo "Error no se pudo conectar";
+        exit;
+    } 
+
+    return $db;
+    
+}
